665e2b43a8e8c8c9337b7c30f1a118c80d7620e5
loren <lorenmichaelh@gmail.com>
2022-03-14 14:20:07 -0500

    commit tree 4d3b36962c6bcdc72ef4d54048f6d6616de009b0

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,21 +3,55 @@ import React, { useState } from "react";
 function Form() {
   const [firstName, setFirstName] = useState("Sylvia");
   const [lastName, setLastName] = useState("Woods");
+  // add this next variable for the display below
+  const [submittedData, setSubmittedData] = useState([]);
+
+  function handleSubmit(event) {
+    event.preventDefault(); //prevent the automatic post
+
+    const formData = {  //assemble the data from the form using the values from state
+      firstName: firstName,
+      lastName: lastName,
+    };
+
+    // props.sendFormDataSomewhere(formData);
+        // since we don't have a backend set up, let's just display the data from state instead.
+
+    const dataArray = [...submittedData, formData]; //add our new data onto the state
+
+    setSubmittedData(dataArray); //update state with the new data
+
+    setFirstName(""); //reset the forms to be blank (we can't use event.target.reset() here because that vanilla JS method doesn't update state!)
+    setLastName("");
+  };
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
-  }
+  };
 
   function handleLastNameChange(event) {
     setLastName(event.target.value);
-  }
+  };
+
+  // map our data to be displayed below
+  const listOfSubmissions = submittedData.map((data, index) => {
+    return (
+      <div key={index}>
+        {data.firstName} {data.lastName}
+      </div>
+    )
+  });
 
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleFirstNameChange} value={firstName} />
+        <input type="text" onChange={handleLastNameChange} value={lastName} />
+        <button type="submit">Submit</button>
+      </form>
+      <h3>Submissions</h3>
+      {listOfSubmissions}
+    </div>
   );
 }
 
